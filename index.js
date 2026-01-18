e44ad44c58e30568c38c00b01cd8b1b02fde417d
luongvantrang <luongvantrang2020@gmail.com>
2026-01-18 13:41:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -971,7 +971,20 @@ client.on('interactionCreate', async (interaction) => {
     }
   }
 });
+// ===================== KEEP ALIVE (Uptime Robot) =====================
+const http = require('http');
 
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Bot is alive!');
+});
+
+server.listen(3000, () => {
+  console.log('Keep-alive server running on port 3000');
+});
+
+// =====================
 // ===================== LOGIN =====================
 ensureInviteConfig();
 client.login(config.bot.token);
+
